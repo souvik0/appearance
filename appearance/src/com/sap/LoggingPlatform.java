b3c606b9f5b1011b5374736635e8d233aa7bc881
Souvik Sen <sensouvik0112@gmail.com>
2022-04-03 14:38:29 +0530

    Removing appearance

--- appearance/src/com/sap/LoggingPlatform.java
+++
@@ -1,7 +0,0 @@
-package com.sap;
-// This is observer hierarchy
-public interface LoggingPlatform {
-
-    public void write(Loglevel loglevel);
-    public void pushLogToDelivery(LogDelivery delivery);
-}
--- appearance/src/com/whatfix/Client.java
+++
@@ -1,9 +0,0 @@
-package com.whatfix;
-
-public class Client {
-
-    public static void main(String[] args) {
-        BoilerContext boiler = new BoilerContext();
-        boiler.invoke();
-    }
-}
