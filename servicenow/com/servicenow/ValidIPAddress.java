42b4584ee07e87f676afa34cb0309922a14051fa
Souvik Sen <sensouvik0112@gmail.com>
2022-10-21 23:09:48 +0530

    Added new class to find valid ip addresses from a given string

---
+++ servicenow/com/servicenow/ValidIPAddress.java
@@ -0,0 +1,66 @@
+package com.servicenow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* Time Complexity: O(1)
+   Space Complexity: O(1)
+*/
+public class ValidIPAddress {
+
+    public static void main(String[] args) {
+        String ipString = "255255255";
+        System.out.println(validIpAddress(ipString).toString());
+    }
+
+    public static List<String> validIpAddress(String ipString) {
+        List<String> ipAddressFound = new ArrayList<String>();
+
+        for (int i = 1; i < Math.min(ipString.length(), 4); i++) {
+            String[] currentIpAddressParts = new String[] {"", "", "", ""};
+
+            currentIpAddressParts[0] = ipString.substring(0, i);
+            if (!isValidPart(currentIpAddressParts[0])) {
+                continue;
+            }
+
+            for (int j = i + 1; j < i + Math.min(ipString.length() - i, 4); j++) {
+                currentIpAddressParts[1] = ipString.substring(i, j);
+                if (!isValidPart(currentIpAddressParts[1])) {
+                    continue;
+                }
+
+                for (int k = j + 1; k < j + Math.min(ipString.length() - j, 4); k++) {
+                    currentIpAddressParts[2] = ipString.substring(j, k);
+                    currentIpAddressParts[3] = ipString.substring(k);
+                    if (isValidPart(currentIpAddressParts[2]) && isValidPart(currentIpAddressParts[3])){
+                        ipAddressFound.add(joinAllParts(currentIpAddressParts));
+                    }
+                }
+            }
+        }
+
+        return ipAddressFound;
+    }
+
+    public static boolean isValidPart(String ipString) {
+        Integer stringAsInt = Integer.parseInt(ipString);
+        if (stringAsInt > 255) {
+            return false;
+        }
+
+        // Dealing with leading zeroes
+        return ipString.length() == stringAsInt.toString().length();
+    }
+
+    public static String joinAllParts(String[] ipAddressParts) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ipAddressParts.length; i++) {
+             sb.append(ipAddressParts[i]);
+             if (i < ipAddressParts.length - 1) {
+                 sb.append(".");
+             }
+        }
+        return sb.toString();
+    }
+}
